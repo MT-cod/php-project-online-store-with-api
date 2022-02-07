17257942e2eaae95fd846f97589c09559ac5b96d
MT-cod <mammmoth@list.ru>
2022-02-07 16:36:16 +0300

    deploing to heroku (adjusting the order of migrations)

--- database/migrations/2021_08_30_163000_create_categories_table.php
+++ database/migrations/2021_08_30_163000_create_categories_table.php
@@ -13,7 +13,7 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories', static function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->unsignedInteger('parent_id')->nullable()->default(0);
